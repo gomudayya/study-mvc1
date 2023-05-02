b284b511456359317ae3f991c1cd28192daa67bb
gomudayya <129571789+gomudayya@users.noreply.github.com>
2023-05-02 15:58:35 +0900

    Add files via upload

---
+++ servlet/src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,30 @@
+package hello.servlet.basic.response;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "responseHeaderServlet", urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // [status-line]
+        resp.setStatus(HttpServletResponse.SC_BAD_REQUEST); // http 상태코드 (200, 300, 400,...)
+
+        // [response-headers]
+        resp.setHeader("Content-Type", "text/plain;charset=utf-8");
+        resp.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        resp.setHeader("Pragma", "no-cache");
+        resp.setHeader("my-header", "hello");
+
+        PrintWriter writer = resp.getWriter();
+        writer.println("ㅎㅇ요");
+    }
+}
